6655fa6b74384f7dbffae68e584fc2041ab48be2
Lin Yang <lin.a.yang@intel.com>
2017-07-31 20:47:18 -0700

    Add basic code structure for OSC plugin
    Change-Id: Idaf48c8b2e5c9b370e69520578d91c8ddb30fd74

---
+++ rsdclient/v1/node.py
@@ -0,0 +1,32 @@
+#   Copyright 2017 Intel, Inc.
+#
+#   Licensed under the Apache License, Version 2.0 (the "License"); you may
+#   not use this file except in compliance with the License. You may obtain
+#   a copy of the License at
+#
+#        http://www.apache.org/licenses/LICENSE-2.0
+#
+#   Unless required by applicable law or agreed to in writing, software
+#   distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+#   WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+#   License for the specific language governing permissions and limitations
+#   under the License.
+#
+
+from rsdclient.common import base
+
+
+class NodeManager(base.Manager):
+    # resource_class = Node
+    _resource_name = 'nodes'
+
+    def get(self, node_id):
+        return self.client.get_node(node_id)
+
+    def delete(self, node_id):
+        return self.client.get_node(node_id).delete_node()
+
+    def compose(self, properites):
+        # TODO(lin.yang): should return id of new composed node, like
+        # 'redfish/v1/Nodes/1'
+        return self.client.get_node_collection().compose_node(properites)
