c09c76951eeed1acad7cc2db8d67508f7916f5bd
Lin Yang <lin.a.yang@intel.com>
2017-08-24 12:00:29 -0700

    Add list storage services command
    Add new command 'rsd storage list' to allow user to list all storage
    services brief info like below shows.
    
    $ openstack rsd storage list
    +----------+-----------------+-----------------------------+
    | Identity |       Name      |         Description         |
    +----------+-----------------+-----------------------------+
    |    1     | Storage Service | Storage Service for Testing |
    +----------+-----------------+-----------------------------+
    
    Change-Id: Id54e630881de4c79f731ca93e6bf7a3c18fe6589

---
+++ rsdclient/tests/v1/test_storage.py
@@ -0,0 +1,51 @@
+#   Copyright 2017 Intel, Inc.
+#
+#   Licensed under the Apache License, Version 2.0 (the "License"); you may
+#   not use this file except in compliance with the License. You may obtain
+#   a copy of the License at
+#
+#        http://www.apache.org/licenses/LICENSE-2.0
+#
+#   Unless required by applicable law or agreed to in writing, software
+#   distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+#   WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+#   License for the specific language governing permissions and limitations
+#   under the License.
+#
+
+import mock
+import testtools
+
+from rsdclient.tests.common import fakes
+from rsdclient.v1 import storage
+
+
+class NodeTest(testtools.TestCase):
+
+    def setUp(self):
+        super(NodeTest, self).setUp()
+        self.client = mock.Mock()
+        self.client._storage_service_path = '/redfish/v1/Services'
+        self.mgr = storage.StorageManager(self.client)
+
+    def test_list_storage(self):
+        mock_storage_collection = mock.Mock()
+        mock_storage_collection.members_identities = \
+            ('/redfish/v1/Services/1',)
+        self.mgr.client.get_storage_service_collection.return_value = \
+            mock_storage_collection
+        self.mgr.client.get_storage_service.return_value = \
+            fakes.FakeStorageSerice()
+
+        expected = (
+            '+----------+-----------------+-----------------------------+\n'
+            '| Identity |       Name      |         Description         |\n'
+            '+----------+-----------------+-----------------------------+\n'
+            '|    1     | Storage Service | Storage Service for Testing |\n'
+            '+----------+-----------------+-----------------------------+')
+
+        result = self.mgr.list()
+        self.mgr.client.get_storage_service_collection.assert_called_once()
+        self.mgr.client.get_storage_service.assert_called_once_with(
+            '/redfish/v1/Services/1')
+        self.assertEqual(str(result), expected)
